5a0f3c6b58a4a319a78b8793dcc56ac9ba46ab49
gs-snasika <snasika@gainsight.com>
2016-08-14 08:50:22 +0530

    test project

---
+++ test/src/com/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Snasika on 14/08/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is a test project");
+    }
+}
